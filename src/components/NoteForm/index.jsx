0ee9427734b63b36acd2ca93c8120546f4d34889
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-08-04 16:17:36 -0300

    refactor: refatorando regra de formulário para hook form/yup

--- src/components/NoteForm/index.jsx
+++ src/components/NoteForm/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
 
-const NoteForm = ({ handleSubmit, formData, setFormData }) => {
+const NoteForm = ({submit}) => {
+  const formSchema = yup.object().shape({
+    title: yup.string().required('O título é obrigatório!'),
+    text: yup.string().required('O texto é obrigatório!').min(50, "A mensagem precisa conter pelo menos 50 caracteres.")
+  })
+  const { register, handleSubmit, formState: { errors }} = useForm({
+    resolver: yupResolver(formSchema),
+  })
   return (
     <div>
         <form
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(submit)}
           style={{
             display: "flex",
             flexDirection: "column",
@@ -14,15 +24,13 @@ const NoteForm = ({ handleSubmit, formData, setFormData }) => {
         >
           <input
             type="text"
-            value={formData.title}
-            onChange={(e) =>
-              setFormData({ ...formData, title: e.target.value })
-            }
+            {...register('title')}
           />
+          {errors.title?.message}
           <textarea
-            value={formData.text}
-            onChange={(e) => setFormData({ ...formData, text: e.target.value })}
+            {...register('text')}
           ></textarea>
+          {errors.text?.message}
           <button>Adicionar a nota</button>
         </form>
       </div>
